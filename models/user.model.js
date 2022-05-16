7716b50d58c4dfe356093778735a95e22052a9a4
Fabio Angelici <fabio.angelici@outlook.it>
2022-05-17 01:55:56 +0200

    Iniziato modello user

---
+++ models/user.model.js
@@ -0,0 +1,14 @@
+class User {
+    constructor(email, password, fullname, street, cap, city) {
+        this.email = email;
+        this.password = password;
+        this.name = fullname;
+        this.address = {
+            street,
+            cap,
+            city,
+        };
+
+        signup()
+    }
+}
